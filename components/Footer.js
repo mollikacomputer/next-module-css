adc9394aa390944989d77cf6518e72cc6b6c5814
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-12 18:35:22 +0600

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '@/styles/footer.module.css';
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <h2 className="text-2xl">Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/header.module.css'
+const Header = () => {
+    return (
+        <div className={styles.header} >
+            <h2 className="text-2xl"> Header </h2>            
+        </div>
+    );
+};
+
+export default Header;
